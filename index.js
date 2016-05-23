104f62341a767797124570132b79534a6a160185
Albin Mattsson <albin@mattsson.io>
2016-05-23 07:44:36 +0200

    🚀 Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const init = require('./lib/init')
+const start = require('./lib/start')
+const stop = require('./lib/stop')
+const selfTest = require('./lib/self-test')
+
+module.exports = {
+  init,
+  start,
+  stop,
+  selfTest
+}
